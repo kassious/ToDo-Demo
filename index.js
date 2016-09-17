05088b3d814fd30dda17aa03d6bbf33c1d8dacb2
kassious <kass.mabo@gmail.com>
2016-09-18 01:15:59 +0300

    add.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var add = function (a, b) {
-    "use strict";
-    return a+b;
-}
 
-console.log (add(5,2));
+
+var express = require('express')
+var add = require('./add');
+
+var result = add(5,2);
+var app = express()
+
+app.get('/', function (req, res) {
+    res.send('result is'+" " + result);
+})
+
+app.listen(3000)
